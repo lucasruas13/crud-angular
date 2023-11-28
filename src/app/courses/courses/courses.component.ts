0d1a6865a70b03cd985119a3aa104fb22be0b807
Lucas Ruas <38050542+lucasruas13@users.noreply.github.com>
2023-11-28 16:06:07 -0300

    Realizada criação de um Service (Angular)

--- src/app/courses/courses/courses.component.ts
+++ src/app/courses/courses/courses.component.ts
@@ -1,3 +1,4 @@
+import { CoursesService } from './../services/courses.service';
 import { Component, OnInit } from '@angular/core';
 import { Course } from '../model/course';
 
@@ -8,13 +9,12 @@ import { Course } from '../model/course';
 })
 export class CoursesComponent implements OnInit {
 
-  courses: Course[] = [
-    { _id: '1' , name: 'Angular' , category: 'front-end' },
-    { _id: '2' , name: 'JavaScript' , category: 'back-end' }
-  ];
+  courses: Course[] = [];
   displayedColumns = ['name' , 'category'];
 
-  constructor() { }
+  constructor(private CoursesService: CoursesService) {
+    this.courses = this.CoursesService.list();
+  }
 
   ngOnInit(): void {
 
