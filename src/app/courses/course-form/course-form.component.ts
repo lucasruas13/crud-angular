1dbfef2286581e8633324cc078340d5664745d78
Lucas Ruas <38050542+lucasruas13@users.noreply.github.com>
2023-12-01 17:53:39 -0300

    Atualizado style.scss conforme Angular v.17

--- src/app/courses/course-form/course-form.component.ts
+++ src/app/courses/course-form/course-form.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-course-form',
@@ -7,6 +8,19 @@ import { Component } from '@angular/core';
   templateUrl: './course-form.component.html',
   styleUrl: './course-form.component.scss'
 })
-export class CourseFormComponent {
+export class CourseFormComponent implements OnInit {
+
+  form: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) {
+    this.form = this.formBuilder.group({
+      name: [null],
+      category: [null]
+    });
+  }
+
+  ngOnInit(): void {
+
+  }
 
 }
