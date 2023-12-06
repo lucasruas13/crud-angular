954dd2f3c6470f9375b83751b38428dcef92257f
Lucas Ruas <38050542+lucasruas13@users.noreply.github.com>
2023-12-06 20:08:05 -0300

    Corrigido erro de rota para new courses

--- src/app/courses/courses/courses.component.ts
+++ src/app/courses/courses/courses.component.ts
@@ -44,7 +44,7 @@ export class CoursesComponent implements OnInit {
   }
 
   onAdd() {
-    this.router.navigate(['new']), {relativeTo: this.route};
+    this.router.navigate(['new'], {relativeTo: this.route});
   }
 
 }
